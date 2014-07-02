c0f0ef67f78636c801ae538006c4b89e565d458e
Luca Fabbri <luca@keul.it>
2014-07-02 11:38:36 +0200

    import iniziale

---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.1.0'
+
+setup(name='unipdgest.portlet.navigation',
+      version=version,
+      description="Navigation with accordion",
+      long_description=open("README.rst").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.rst")).read(),
+      # Get more strings from
+      # http://pypi.python.org/pypi?:action=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        ],
+      keywords='plone navigation accordion',
+      author='RedTurtle Technology',
+      author_email='sviluppoplone@redturtle.it',
+      url='http://bitbucket.org/redturtle/unipdgest.portlet.navigation',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['unipdgest', 'unipdgest.portlet'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'plone.app.portlets',
+      ],
+      entry_points="""
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
